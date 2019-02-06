6725e8289a019976f5af58d28f3cee9467caa3b9
SergeyYakimov <teriertis@yandex.ru>
2019-02-06 21:18:12 +0800

    Fixed arrays

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 $is_auth = rand(0, 1);
 
-$user_name = 'Sergey Yakimov';
-$user_image = 'img/user.jpg';
+$user = [
+    'name' => 'Sergey Yakimov',
+    'image' => 'img/user.jpg'
+];
+
 $categories = [
     'boards' => 'Доски и лыжи',
     'attachment' => 'Крепления',
@@ -13,21 +16,46 @@ $categories = [
 ];
 
 $ads_list = [
-    'names' => [
-        '2014 Rossignol District Snowboard',
-        'DC Ply Mens 2016/2017 Snowboard',
-        'Крепления Union Contact Pro 2015 года размер L/XL',
-        'Ботинки для сноуборда DC Mutiny Charocal',
-        'Куртка для сноуборда DC Mutiny Charosal',
-        'Маска Oakley Canopy',
+	[
+		'name' => '2014 Rossignol District Snowboard',
+		'category' => 'Доски и лыжи',
+        'price' => 10999,
+        'url' => 'img/lot-1.jpg'
+	],
+	[
+		'name' => 'DC Ply Mens 2016/2017 Snowboard',
+		'category' => 'Доски и лыжи',
+        'price' => 159999,
+        'url' => 'img/lot-2.jpg'
     ],
-    'categories' => ['Доски и лыжи', 'Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Разное'],
-    'prices' => [10999, 159999, 8000, 10999, 7500, 5400],
-    'urls' => ['img/lot-1.jpg', 'img/lot-2.jpg', 'img/lot-3.jpg', 'img/lot-4.jpg', 'img/lot-5.jpg', 'img/lot-6.jpg']
+    [
+        'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+        'category' => 'Крепления',
+        'price' => 8000,
+        'url' => 'img/lot-3.jpg'
+    ],
+    [
+        'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
+        'category' => 'Ботинки',
+        'price' => 10999,
+        'url' => 'img/lot-4.jpg'
+    ],
+    [
+        'name' => 'Куртка для сноуборда DC Mutiny Charosal',
+        'category' => 'Одежда',
+        'price' => 7500,
+        'url' => 'img/lot-5.jpg'
+    ],
+    [
+        'name' => 'Маска Oakley Canopy',
+        'category' => 'Разное',
+        'price' => 5400,
+        'url' => 'img/lot-6.jpg'
+    ]
 ];
 function format_price($price) {
     $integer_price = ceil($price);
-    return number_format($integer_price, 0, '.', ' ') . ' <b class="rub">р</b>';
+    return number_format($integer_price, 0, '.', ' ') . '<b class="rub">р</b>';
 }
 ?>
 <!DOCTYPE html>
@@ -56,10 +84,10 @@ function format_price($price) {
         <nav class="user-menu">
         <?php if($is_auth === 1):?>
             <div class="user-menu__image">
-                <img src="<?=$user_image;?>" width="40" height="40" alt="Пользователь">
+                <img src="<?=$user['image'];?>" width="40" height="40" alt="Пользователь">
             </div>
             <div class="user-menu__logged">
-                <p><?=htmlspecialchars($user_name);?></p>
+                <p><?=htmlspecialchars($user['name']);?></p>
                 <a href="#">Выйти</a>
             </div>
         <?php else:?>
@@ -93,19 +121,18 @@ function format_price($price) {
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-            <?php $index = 0;
-            while($index < count($ads_list['names'])):?>
+            <?php foreach($ads_list as $key => $value):?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?=$ads_list['urls'][$index];?>" width="350" height="260" alt="">
+                    <img src="<?=$value['url'];?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?=$ads_list['categories'][$index];?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=htmlspecialchars($ads_list['names'][$index]);?></a></h3>
+                    <span class="lot__category"><?=$value['category'];?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=htmlspecialchars($value['name']);?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=format_price($ads_list['prices'][$index]); ?></span>
+                            <span class="lot__cost"><?=format_price($value['price']);?></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -113,8 +140,7 @@ function format_price($price) {
                     </div>
                 </div>
             </li>
-            <?php $index++;
-            endwhile;?>
+            <?php endforeach;?>
         </ul>
     </section>
 </main>
