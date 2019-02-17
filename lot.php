8a9691bc7dc2cf243010842c9da3c45db47fa612
SergeyYakimov <teriertis@yandex.ru>
2019-02-18 00:20:14 +0800

    Deleted lots.php, added setting the status of 404 in case of incorrect request

--- lot.php
+++ lot.php
@@ -2,9 +2,14 @@
 
 require_once('init.php');
 
-$id = intval($_GET['id']);
-if (!isset($id)) {
+if (isset($_GET['id'])) {
+    $id = intval($_GET['id']);
+} else {
+    header("HTTP/1.0 404 Not Found");
     $page_content = include_template('error.php', ['categories' => $categories]);
+    $layout_content = include_template('layout.php', ['content' => $page_content, 'categories' => $categories, 'name_page' => '404 - Страница не найдена', 'user' => $user]);
+    print($layout_content);
+    die();
 }
 
 $sql_lot = "SELECT lot_id, description, step_rate, completion_date, title, start_price, image, COALESCE(MAX(r.rate), start_price) AS price, c.name FROM lots
@@ -14,13 +19,16 @@ $sql_lot = "SELECT lot_id, description, step_rate, completion_date, title, start
             GROUP BY lot_id";
 $result_lot = mysqli_query($link, $sql_lot);
 
-if (mysqli_num_rows($result_lot)) {
-    $lot = mysqli_fetch_assoc($result_lot);
-    $page_content = include_template('lot.php', ['categories' => $categories, 'lot' => $lot]);
-} else {
+if (!mysqli_num_rows($result_lot)) {
+    header("HTTP/1.0 404 Not Found");
     $page_content = include_template('error.php', ['categories' => $categories]);
+    $layout_content = include_template('layout.php', ['content' => $page_content, 'categories' => $categories, 'name_page' => '404 - Страница не найдена', 'user' => $user]);
+    print($layout_content);
+    die();
 }
 
+$lot = mysqli_fetch_assoc($result_lot);
+$page_content = include_template('lot.php', ['categories' => $categories, 'lot' => $lot]);
 $layout_content = include_template('layout.php', ['content' => $page_content, 'categories' => $categories, 'name_page' => $lot['title'], 'user' => $user]);
 print($layout_content);
 ?>
