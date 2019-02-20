e18b56e1e2a59f50757652a1d21942086b58cf1b
SergeyYakimov <teriertis@yandex.ru>
2019-02-20 23:09:32 +0800

    Deleted function from lot.php

--- lot.php
+++ lot.php
@@ -2,17 +2,13 @@
 
 require_once('init.php');
 
-function show_error ($categories, $user) {
+if (isset($_GET['id'])) {
+    $id = intval($_GET['id']);
+} else {
     header("HTTP/1.0 404 Not Found");
     $page_content = include_template('error.php', ['categories' => $categories]);
     $layout_content = include_template('layout.php', ['content' => $page_content, 'categories' => $categories, 'name_page' => '404 - Страница не найдена', 'user' => $user]);
     print($layout_content);
-}
-
-if (isset($_GET['id'])) {
-    $id = intval($_GET['id']);
-} else {
-    show_error($categories, $user);
     die();
 }
 
@@ -24,7 +20,10 @@ $sql_lot = "SELECT lot_id, description, step_rate, completion_date, title, start
 $result_lot = mysqli_query($link, $sql_lot);
 
 if (!mysqli_num_rows($result_lot)) {
-    show_error($categories, $user);
+    header("HTTP/1.0 404 Not Found");
+    $page_content = include_template('error.php', ['categories' => $categories]);
+    $layout_content = include_template('layout.php', ['content' => $page_content, 'categories' => $categories, 'name_page' => '404 - Страница не найдена', 'user' => $user]);
+    print($layout_content);
     die();
 }
 
