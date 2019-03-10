a88c7daf6e410d6b410ddc51f865ff67c6a64799
SergeyYakimov <teriertis@yandex.ru>
2019-03-10 22:57:54 +0800

    Added documentation of functions

--- functions.php
+++ functions.php
@@ -1,10 +1,23 @@
 <?php
 require_once('mysql_helper.php');
 
+/**
+* Функция для форматирования суммы
+* @param integer|float $price Цена в числовом формате
+* @return string Отформатированная цена в денежном формате
+*/
+
 function format_price($price) {
     $integer_price = ceil($price);
     return number_format($integer_price, 0, '.', ' ') . '<b class="rub">р</b>';
 }
+
+/**
+* Функция-шаблонизатор
+* @param string $name Имя файла шаблона
+* @param array $data Данные, передаваемые в шаблон
+* @return string Готовая часть разметки страницы
+*/
 
 function include_template($name, $data) {
     $name = 'templates/' . $name;
@@ -23,6 +36,12 @@ function include_template($name, $data) {
     return $result;
 }
 
+/**
+* Отображение окончания лота в соответствующем формате
+* @param string $closing_time Дата и время полученные из БД
+* @return string Отображение времени завершения лота в подобранном формате
+*/
+
 function get_time_till_closing_time($closing_time) {
     $diff_time = strtotime($closing_time) - time();
     $sec_in_day = 86400;
@@ -37,6 +56,13 @@ function get_time_till_closing_time($closing_time) {
     return $format_time;
 }
 
+/**
+* Функция, определяющая имеется ли в базе данных пользователь с таким же email
+* @param mysqli $link Ресурс подключения к базе данных
+* @param string $email E-mail адрес
+* @return bool true - пользователь с указанным e-mail найден, false - не найден
+*/
+
 function is_registered_email($link, $email) {
     $result = 0;
     $sql = "SELECT user_id FROM users WHERE email = '$email'";
@@ -48,6 +74,14 @@ function is_registered_email($link, $email) {
     }
     return !($result === 0);
 }
+
+/**
+ * Получает данные конкретного пользователя из базы данных
+ *
+ * @param mysqli $link Ресурс подключения к базе данных
+ * @param array $object Ассоциативный массив, задающий значения для поиска
+ * @return array Массив данных пользователя
+ */
 
 function identify_user($link, $object) {
     $result = [];
@@ -65,6 +99,14 @@ function identify_user($link, $object) {
     return $result;
 }
 
+/**
+ * Получает из базы данных массив ставок для конкретного лота
+ *
+ * @param mysqli $link Ресурс подключения к базе данных
+ * @param integer $lot_id id лота
+ * @return array Массив ставок для конкретного лота
+ */
+
 function get_rates($link, $lot_id) {
     $result = [];
     $sql_rates =
@@ -80,6 +122,14 @@ function get_rates($link, $lot_id) {
     }
     return $result;
 }
+
+/**
+ * Выполняет запись новой строки в таблицу ставок базы данных на основе переданных данных и возвращает id этой строки
+ *
+ * @param mysqli $link Ресурс соединения с базой данных
+ * @param array $information Массив данных для записи новой ставки в базу данных
+ * @return integer id записанной строки
+ */
 
 function add_rate($link, $information) {
     $rate_id = '';
@@ -97,6 +147,14 @@ function add_rate($link, $information) {
     }
     return $rate_id;
 }
+
+/**
+ * Добавляет к числу соответствующее наименование в правильном падеже
+ *
+ * @param integer $value Числовое значение
+ * @param string $indicator Индикатор, указывающий на конкретные варианты склонения существительного
+ * @return string Наименование в правильном падеже
+ */
 
 function set_format_phrase($value, $indicator) {
     $result = '';
@@ -123,6 +181,13 @@ function set_format_phrase($value, $indicator) {
     }
     return $result;
 }
+
+/**
+ * Представляет период времени, прошедший с момента добавления ставки в необходимом формате
+ *
+ * @param string $time Время добавления ставки
+ * @return string Отформатированное время добавления ставки
+ */
 
 function get_rate_add_time($time) {
     $add_time = strtotime($time);
@@ -153,6 +218,14 @@ function get_rate_add_time($time) {
     return $result;
 }
 
+/**
+ * Получает из базы данных конкретную категорию из таблицы категорий
+ *
+ * @param mysqli $link Ресурс подключения к базе данных
+ * @param array $category Ассоциативный массив, задающий значения для поиска
+ * @return array Массив категорий
+ */
+
 function get_categories($link, $category) {
     $result = [];
     $sql_category = '';
@@ -168,6 +241,18 @@ function get_categories($link, $category) {
     }
     return $result;
 }
+
+/**
+ * Возвращает либо массив лотов либо количество лотов
+ *
+ * @param mysqli $link Ресурс подключения к базе данных
+ * @param integer $limit Количество лотов, отображаемое на странице
+ * @param string|bool $search Текст запроса для поиска
+ * @param integer|bool $category_id id категории лота
+ * @param integer|bool $page_id id страницы при осуществлении навигации
+ * @param bool $records Параметр, определяющий тип возвращаемого функцией результата (false - массив, true - количество)
+ * @return array|integer Массив лотов|количество лотов
+ */
 
 function get_lots($link, $limit, $search = false, $category_id = false, $page_id = false, $records = false) {
     $result = [];
@@ -202,23 +287,29 @@ function get_lots($link, $limit, $search = false, $category_id = false, $page_id
     return $records ? $count : $result;
 }
 
+/**
+ * Возвращает массив данных для блока пагинации
+ *
+ * @param integer $pages_count Общее количество страниц
+ * @param integer $present_page Номер текущей страницы
+ * @param array $search_information Массив исходных get-параметров страницы
+ * @param integer $max_pages Максимальное возможное количество страниц
+ * @return array Двумерный массив данных, каждый элемент которого содержит номер страницы, css-класс каждого элемента списка и текст атрибута href
+ */
+
 function get_pagination_information($pages_count, $present_page, $search_information, $max_pages) {
     $previous ='';
     $next ='';
-
     if ($pages_count <= 1) {
         return [];
     }
-
     $max_pages = $pages_count < $max_pages ? $pages_count : $max_pages;
     $pagination_information = [];
-
     $left = $current_page - 1;
     $right = $pages_count - $present_page;
     $middle = (int) ceil($max_pages / 2);
     $left_min = $middle - 1;
     $right_min = $max_pages - $middle;
-
     if ($present_page > 1) {
         $search_information['page'] = $present_page - 1;
         $previous = ' href="?' . http_build_query($search_information) . '"';
@@ -227,17 +318,14 @@ function get_pagination_information($pages_count, $present_page, $search_informa
         $search_information['page'] = $present_page + 1;
         $next = ' href="?' . http_build_query($search_information) . '"';
     }
-
     $pagination_information[0] = ['page_number' => 'Назад', 'class' => ' pagination-item-prev', 'href' => $previous];
-    $j = 1;
 
-    while ($j <= $max_pages) {
-        $page_number = $j;
-
+    for ($i = 1; $i <= $max_pages; $i++) {
+        $page_number = $i;
         if ($left > $left_min && $right > $right_min) {
-            $page_number = $j + $present_page - $middle;
+            $page_number = $i + $present_page - $middle;
         } else if ($right <= $right_min) {
-            $page_number = $j + $pages_count - $max_pages;
+            $page_number = $i + $pages_count - $max_pages;
         }
 
         $page_href = '';
@@ -247,17 +335,19 @@ function get_pagination_information($pages_count, $present_page, $search_informa
             $page_href = ' href="?' . http_build_query($search_information) . '"';
             $class = '';
         }
-        $pagination_information[$j] = [
-            'page_number' => $page_number,
-            'class' => $class,
-            'href' => $page_href
-        ];
-
-        $j++;
+        $pagination_information[$i] = ['page_number' => $page_number, 'class' => $class, 'href' => $page_href];
     }
     $pagination_information[$max_pages + 1] = ['page_number' => 'Вперед', 'class' => ' pagination-item-next', 'href' => $next];
     return $pagination_information;
 }
+
+/**
+ * Функция получает массив ставок, которые сделал конкретный пользователь
+ *
+ * @param mysqli $link Ресурс подключения к базе данных
+ * @param int $user_id Идентификатор пользователя
+ * @return array Массив ставок конкретного пользователя
+ */
 
 function get_user_rates($link, $user_id) {
     $result = [];
@@ -278,6 +368,13 @@ function get_user_rates($link, $user_id) {
     return $result;
 }
 
+/**
+ * Представляет время до окончания торгов по лоту в удобном формате либо указывает, что торги закончены
+ *
+ * @param string $time Дата окончания торгов по лоту
+ * @return string Время до окончания торгов либо указание на то, что торги закончены
+ */
+
 function get_time_of_end_lot ($time) {
     $completion_time = strtotime($time);
 
@@ -297,6 +394,14 @@ function get_time_of_end_lot ($time) {
     }
     return $result;
 }
+
+/**
+ * Получает из базы данных записи для пользователя, являющегося победителем по конкретным торгам
+ *
+ * @param mysqli $link Ресурс подключения к базе данных
+ * @param array $user Ассоциативный массив, задающий значения для поиска
+ * @return array Массив записей из базы данных для пользователя, являющегося победителем торгов
+ */
 
 function get_winner($link, $user) {
     $result = [];
